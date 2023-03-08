b9859c078e9764782219b868e45177631d664295
David Rhodes <david@espressocode.tech>
2023-03-08 11:51:55 -0600

    algolia connected

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,43 @@
 import Head from 'next/head';
-import ProductListing from '@components/ProductListing';
+import { useState } from 'react';
+import HitListing from '@components/HitListing';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
 import { getProductList } from 'netlify/functions/utils/getProductList.js';
-import algoliasearch from 'algoliasearch';
 import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-hooks-web';
+import searchClient from '../algolia';
 
-const algoliaSearchApiKey = process.env.ALGOLIA_SEARCH_API_KEY;
-console.log(
-  '🚀 ~ file: index.js:10 ~ algoliaSearchApiKey:',
-  algoliaSearchApiKey
-);
-const appId = process.env.ALGOLIA_APP_ID;
-const searchIndex = process.env.ALGOLIA_SEARCH_INDEX;
-
-const searchClient = algoliasearch(appId, algoliaSearchApiKey);
-const index = searchClient.initIndex(searchIndex);
-console.log('🚀 ~ file: index.js:14 ~ index:', index);
+const index = searchClient.initIndex('algolia-index');
 
 function Hit({ hit }) {
-  return (
-    <article>
-      <img src={hit.image} alt={hit.name} />
-      <p>{hit.categories[0]}</p>
-      <h1>{hit.name}</h1>
-      <p>${hit.price}</p>
-    </article>
-  );
+  return <HitListing hit={hit} />;
 }
 
 export default function Home({ products }) {
+  const [searchState, setSearchState] = useState({});
+
+  const handleSearchStateChange = (newSearchState) => {
+    setSearchState(newSearchState);
+  };
+
   return (
     <>
       <Head>
-        <title>Cheese and Meat Shop</title>
+        <title>Life Fitness Shop</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
       <main>
-        <InstantSearch searchClient={searchClient} indexName={index}>
+        <InstantSearch
+          searchClient={searchClient}
+          indexName={'algolia-index'}
+          searchState={searchState}
+          onSearchStateChange={handleSearchStateChange}
+        >
           <SearchBox />
           <Hits hitComponent={Hit} />
         </InstantSearch>
-        <ul className="product-grid">
-          {products.map((p, index) => {
-            return <ProductListing key={`product${index}`} product={p.node} />;
-          })}
-        </ul>
       </main>
 
       <Footer />
