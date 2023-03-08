35ef0d357d264c47c0ccfad22606dc9b51739014
David Rhodes <david@espressocode.tech>
2023-03-08 11:57:39 -0600

    corrects async useEffect

--- components/Cart.js
+++ components/Cart.js
@@ -1,7 +1,7 @@
-import { useState, useEffect } from "react";
-import { useAppContext } from "../state";
-import CartTable from "./CartTable";
-import CartTotal from "./CartTotal";
+import { useState, useEffect } from 'react';
+import { useAppContext } from '../state';
+import CartTable from './CartTable';
+import CartTotal from './CartTotal';
 
 export default function Cart() {
   const [showProducts, setShowProducts] = useState(true);
@@ -9,26 +9,30 @@ export default function Cart() {
   const [cost, setCost] = useState({});
   const { cartId, setCartId } = useAppContext();
 
-  useEffect(async () => {
-    const localCart = cartId;
+  useEffect(() => {
+    async function fetchData() {
+      const localCart = cartId;
 
-    if (localCart === null) {
-      setShowProducts(false);
-    } else {
-      setCartId(localCart);
-      const response = await fetch("/.netlify/functions/get-cart", {
-        method: "post",
-        body: JSON.stringify({
-          cartId: localCart,
-        }),
-        headers: { "Content-Type": "application/json" },
-      });
-      const json = await response.json();
-      setProducts(json?.cart?.lines.edges);
-      setCost(json?.cart?.estimatedCost);
-      return json;
+      if (localCart === null) {
+        setShowProducts(false);
+      } else {
+        setCartId(localCart);
+        const response = await fetch('/.netlify/functions/get-cart', {
+          method: 'post',
+          body: JSON.stringify({
+            cartId: localCart,
+          }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+        const json = await response.json();
+        setProducts(json?.cart?.lines.edges);
+        setCost(json?.cart?.estimatedCost);
+        return json;
+      }
     }
-  }, []);
+
+    fetchData();
+  }, [cartId, setCartId]);
 
   return (
     <div>
