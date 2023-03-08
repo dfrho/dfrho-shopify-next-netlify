a710f4919e4c3372334829a8019e85086239db6a
David Rhodes <david@espressocode.tech>
2023-03-08 10:53:58 -0600

    test

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,37 @@
-import Head from "next/head";
-import ProductListing from "@components/ProductListing";
-import Header from "@components/Header";
-import Footer from "@components/Footer";
-import { getProductList } from "netlify/functions/utils/getProductList.js";
+import Head from 'next/head';
+import ProductListing from '@components/ProductListing';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+import { getProductList } from 'netlify/functions/utils/getProductList.js';
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-hooks-web';
+// require('dotenv').config();
+
+console.log(
+  '🚀 ~ file: index.js:11 ~ process.env.ALGOLIA_APP_ID:',
+  process.env.ALGOLIA_APP_ID
+);
+console.log(
+  '🚀 ~ file: index.js:13 ~ process.env.ALGOLIA_SEARCH_API_KEY:',
+  process.env.ALGOLIA_SEARCH_API_KEY
+);
+const searchClient = algoliasearch(
+  process.env.ALGOLIA_APP_ID,
+  process.env.ALGOLIA_SEARCH_API_KEY
+);
+const index = searchClient.initIndex(process.env.ALGOLIA_SEARCH_INDEX);
+console.log('🚀 ~ file: index.js:14 ~ index:', index);
+
+function Hit({ hit }) {
+  return (
+    <article>
+      <img src={hit.image} alt={hit.name} />
+      <p>{hit.categories[0]}</p>
+      <h1>{hit.name}</h1>
+      <p>${hit.price}</p>
+    </article>
+  );
+}
 
 export default function Home({ products }) {
   return (
@@ -14,6 +43,10 @@ export default function Home({ products }) {
 
       <Header />
       <main>
+        <InstantSearch searchClient={searchClient} indexName={index}>
+          <SearchBox />
+          <Hits hitComponent={Hit} />
+        </InstantSearch>
         <ul className="product-grid">
           {products.map((p, index) => {
             return <ProductListing key={`product${index}`} product={p.node} />;
