ec1fc5bea9967c2387e833d6ee88ee5036107afc
David Rhodes <david@espressocode.tech>
2023-03-09 07:39:24 -0600

    adds _document

---
+++ pages/_document.js
@@ -0,0 +1,39 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.css"
+          />
+          <script
+            type="text/javascript"
+            src="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.js"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                algoliasearchNetlify({
+                  appId: '30UDJGPUBP',
+                  apiKey: process.env.ALGOLIA_SEARCH_API_KEY,
+                  siteId: '9f739a59-c001-4005-8110-306032c23340',
+                  branch: process.env.HEAD,
+                  selector: 'div#search',
+                });
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
