f14f6db2d73a3a418e8bd305a1280187a961da1d
David Rhodes <david@espressocode.tech>
2023-03-09 08:10:10 -0600

    fixes property

--- netlify/functions/queryopenai.js
+++ netlify/functions/queryopenai.js
@@ -3,7 +3,7 @@ const fetch = require('isomorphic-fetch');
 exports.handler = async (event) => {
   const { workoutType, product } = JSON.parse(event.body);
 
-  const query = `LifeFitness is a leading manufacturer of commercial fitness equipment that can be used at home for Performance, HIIT, Strength, Cardio, and Endurance workouts. For the ${product.title} product, show me a ${workoutType} workout.`;
+  const query = `LifeFitness is a leading manufacturer of commercial fitness equipment that can be used at home for Performance, HIIT, Strength, Cardio, and Endurance workouts. For the ${product} product, show me a ${workoutType} workout.`;
   console.log('🚀 ~ file: queryopenai.js:8 ~ exports.handler= ~ query:', query);
 
   const DEFAULT_PARAMS = {
