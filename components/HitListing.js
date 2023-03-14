64e0782480e51382fd2d561b405a40d2bc150b7b
David Rhodes <david@espressocode.tech>
2023-03-14 07:45:00 -0500

    removes log

--- components/HitListing.js
+++ components/HitListing.js
@@ -2,11 +2,16 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 export default function HitListing({ hit }) {
-  let image = hit.images.edges[0].node;
   return (
     <article className="product-card">
       <div className="product-card-frame">
-        <img className="prodimg" src={image.src} alt={image.altText} />
+        <Image
+          className="prodimg"
+          src={hit['Image Src']}
+          alt={hit['Title']}
+          width={500}
+          height={300}
+        />
       </div>
       <div className="product-card-text">
         <h3 className="product-card-title">{hit['Title']}</h3>
